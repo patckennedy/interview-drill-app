d34382a708b98d07c3527f6f21507c513973d8af
patriciakennedy <42810547+patriciakennedy@users.noreply.github.com>
2025-09-16 18:54:27 -0500

    chore: scaffold project with React + Vite, configure TailwindCSS, and verify setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+const App = () => {
+    return (
+        <>
+            <div>App</div>
+            <h1 className="text-3xl font-bold underline">
+                Tailwind is working
+            </h1>
+        </>
+    );
+};
+
+export default App;
